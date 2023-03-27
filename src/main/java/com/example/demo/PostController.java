0762cba3ea2ed269974313357363679def0cfce1
ireensagh <84302872+ireensagh@users.noreply.github.com>
2023-03-27 01:13:55 -0700

    second commit

--- src/main/java/com/example/demo/PostController.java
+++ src/main/java/com/example/demo/PostController.java
@@ -1,25 +1,31 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 public class MyRestController {
 
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
-    public MyRestController(RestTemplateBuilder restTemplateBuilder) {
+
+    public MyRestController(RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
         this.restTemplate = restTemplateBuilder.build();
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/orgs/acc8cc57-ff7c-44c5-9bd6-ab0900fbdc43/repos")
-    public String callExternalApi() {
+    public ResponseEntity<?> callExternalApi() throws IOException {
         String apiUrl = "https://api.omny.fm/orgs/acc8cc57-ff7c-44c5-9bd6-ab0900fbdc43/repos";
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
         String responseBody = responseEntity.getBody();
-        return responseBody;
+        // Convert the response body from String to JSON object
+        Object json = objectMapper.readValue(responseBody, Object.class);
+        return ResponseEntity.ok(json);
     }
 }
 
 
 public class PostController {
 
-    private final String API_URL = "https://jsonplaceholder.typicode.com/posts";
+    private static final  String API_URL = "https://jsonplaceholder.typicode.com/posts";
 
     @Autowired
     private RestTemplate restTemplate;

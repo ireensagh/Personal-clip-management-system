7d75e5752451aadadd1dc9844c3e4cd00ec9a4d6
ireensagh <84302872+ireensagh@users.noreply.github.com>
2023-04-13 02:00:04 -0700

    error fix

--- src/main/java/com/example/demo/client/ProgramList.java
+++ src/main/java/com/example/demo/client/ProgramList.java
@@ -1,8 +1,14 @@
 package com.example.demo.client;
 
 import com.example.demo.entity.Program;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
+
+@RestController
+@RequestMapping("/api")
 public class ProgramList {
 
     private List<Program> items;
